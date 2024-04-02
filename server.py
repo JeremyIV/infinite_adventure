9117e1ec45b013a8a5f171c164e99ef26604513d
Jeremy Vonderfecht <vonder2@pdx.edu>
2024-04-01 18:01:12 -0700

    try using gunicorn

--- server.py
+++ server.py
@@ -158,10 +158,4 @@ def act():
 
 if __name__ == "__main__":
     print("Just started the server script!!")
-    port = os.environ.get("PORT")
-    if port is None:
-        print("No port yo!")
-    else:
-        print(f"Trying to connect on port {port}")
-        print(type(port))
-    app.run(debug=True, port=int(port))
+    app.run(debug=True)
